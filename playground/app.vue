6714177c7fb8fc1641fe828f0502189561411db3
Corentin PERROUX <corentin7301@gmail.com>
2024-09-23 15:52:39 +0200

    inital commit

---
+++ playground/app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    Nuxt module playground!
+    <pre>{{ data }}</pre>
+  </div>
+</template>
+
+<script setup>
+const { data } = await useFetch('/api/customer')
+</script>
---
+++ playground/nuxt.config.ts
@@ -0,0 +1,6 @@
+export default defineNuxtConfig({
+  modules: ['../src/module', '@prisma/nuxt'],
+  myModule: {},
+  devtools: { enabled: true },
+  compatibilityDate: '2024-09-23',
+})
